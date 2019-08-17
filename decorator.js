ad58d27d9a94d8ebcb3bcea3e61502d6f5bb39fd
Denys Samotos <samotos.dan@gmail.com>
2019-08-17 20:50:15 +0300

    ADD decorator pattern

---
+++ decorator.js
@@ -0,0 +1,9 @@
+class SuperAMX_30B extends AMX_30B {
+  constructor() {
+    super();
+    this.gun = '122mm mle. F3'
+    this.gunDamage += 60;
+  }
+}
+
+const modern_AMX_30B = new SuperAMX_30B();
--- factory.js
+++ factory.js
@@ -1,45 +1,61 @@
 class TankFactory {
-  createTank(name, customProps = {}) {
+  createTank(name = '', customProps = {}) {
+    let tank;
+
     switch (name) {
       case 'AMX 30B':
-        return new AMX_30B(customProps);
+        tank = new AMX_30B(customProps);
+        break;
       case 'AMX 50B':
-        return new AMX_50B(customProps);
+        tank = new AMX_50B(customProps);
+        break;
       case 'AMX 13 105':
-        return new AMX_13_105(customProps);
+        tank = new AMX_13_105(customProps);
+        break;
       default:
-        return new AMX_30B(customProps);
+        tank = new AMX_30B(customProps);
     }
+
+    tank.getTanksType = () => tank.type;
+    tank.shot = () => tank.gunDamage;
+
+    return tank;
   }
 }
 
 class AMX_30B {
-  constructor({turret, gun, engine}) {
+  constructor({turret, gun, engine} = {}) {
     this.turret = turret || 'AMX 30B';
     this.gun = gun || '105mm mle. F1';
     this.engine = engine || 'Hispano-Suiza HS 110';
+
     this.type = 'medium';
     this.name = 'AMX 30B';
+    this.gunDamage = 390;
   }
 }
 
 class AMX_50B {
-  constructor({turret, gun, engine}) {
+  constructor({turret, gun, engine} = {}) {
     this.turret = turret || 'AMX 50B';
     this.gun = gun || '120mm SA46';
     this.engine = engine || 'Maybach HL 295F';
+
     this.type = 'heavy';
     this.name = 'AMX 50B';
+    this.gunDamage = 400;
   }
 }
 
 class AMX_13_105 {
-  constructor({turret, gun, engine}) {
+  constructor({turret, gun, engine} = {}) {
     this.turret = turret || 'AMX 13 105';
     this.gun = gun || '105mm D. 1504';
     this.engine = engine || 'SOFAM 8 Gxb';
+
     this.type = 'light';
     this.name = 'AMX 13 105';
+    this.gunDamage = gun ? 410 : 360;
   }
 }
 
@@ -47,4 +63,4 @@ const tankFactory = new TankFactory();
 const amx30B = tankFactory.createTank('AMX 30B', {engine: 'Hispano-Suiza HS 120'});
 const amx50B = tankFactory.createTank('AMX 50B');
 const amx13_105 = tankFactory.createTank('AMX 13 105', {gun: '110mm D. 1504'});
-const franceTank = tankFactory.createTank('franceTank');
+const franceTank = tankFactory.createTank();
